fdd4ce78ebc77dfaebcccaaecc96d3609288484b
jjakubova <jana.jakubova@niceactimize.com>
2024-08-22 17:15:46 +0200

    pop up closing

--- packages/vm-organizer/src/app/component/popappform/popappform.component.ts
+++ packages/vm-organizer/src/app/component/popappform/popappform.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
 import { CrudService } from '../../service/crud.service';
 import { SpotinstService } from '../../service/spotinst.service';
 import { Vm } from '../../model/vm';
 import { FormControl, FormGroup } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
-import {ViewEncapsulation} from '@angular/core';
+import { ViewEncapsulation } from '@angular/core';
 import { concatMap } from 'rxjs';
 
 @Component({
@@ -14,97 +14,113 @@ import { concatMap } from 'rxjs';
   encapsulation: ViewEncapsulation.None
 })
 export class PopappformComponent {
-    loading = false;
+  loading = false;
 
-    vm : Vm = new Vm();
-    isEnabled = false;
-    spotInstancesPartial: { tag: string, groupId: string }[] = [];
-    spotInstancesFull: { tag: string, groupId: string, statefulId: string }[] = [];
+  isDialogInitialized = false;
+  vm: Vm = new Vm();
+  isEnabled = false;
+  spotInstancesPartial: { tag: string, groupId: string }[] = [];
+  spotInstancesFull: { tag: string, groupId: string, statefulId: string }[] = [];
 
-    vmForm = new FormGroup ({
-        vmTag : new FormControl(''),
-        vmPassword : new FormControl(''),
-        vmIpApp : new FormControl(''),
-        vmIpDb : new FormControl(''),
-        vmNote : new FormControl(''),
-        vmStatus : new FormControl('')
-        
-    })
+  vmForm = new FormGroup({
+    vmTag: new FormControl(''),
+    vmPassword: new FormControl(''),
+    vmIpApp: new FormControl(''),
+    vmIpDb: new FormControl(''),
+    vmNote: new FormControl(''),
+    vmStatus: new FormControl('')
 
-    constructor(private crudService : CrudService,
-                private dialogRed : MatDialog,
-                private spotInstService : SpotinstService) {}
+  })
 
-    onSubmit() {
-        this.vm.ipApp = this.vmForm.value.vmIpApp!
-        this.vm.ipDB = this.vmForm.value.vmIpDb!
-        this.vm.id = this.vmForm.value.vmTag!
-        this.vm.password = this.vmForm.value.vmPassword!
-        this.vm.note = ''
-        if (this.vmForm.value.vmStatus === '') {
-            this.vm.status = 'OFF'
-        } else {
-            this.vm.status = this.vmForm.value.vmStatus!
-        }
+  constructor(private crudService : CrudService,
+    private dialogRed : MatDialog,
+    private spotInstService : SpotinstService,
+    private elementRef: ElementRef) { }
 
-        this.addVm(this.vm);
+  ngOnInit() {
+    setTimeout(() => {
+      this.isDialogInitialized = true;
+    }, 200); 
+  }
 
-        this.vm = new Vm();
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    const clickedInside = this.elementRef.nativeElement.contains(event.target);
+    if (this.isDialogInitialized && !clickedInside && !this.loading) {
+      this.closeDialog();
+    }
+  }
+
+  onSubmit() {
+    this.vm.ipApp = this.vmForm.value.vmIpApp!
+    this.vm.ipDB = this.vmForm.value.vmIpDb!
+    this.vm.id = this.vmForm.value.vmTag!
+    this.vm.password = this.vmForm.value.vmPassword!
+    this.vm.note = ''
+    if (this.vmForm.value.vmStatus === '') {
+      this.vm.status = 'OFF'
+    } else {
+      this.vm.status = this.vmForm.value.vmStatus!
     }
 
-    addVm(vm : Vm) {
-        this.loading = true
-        this.crudService.addVm(vm).subscribe(
-            {
-                next: res => {
-                  this.spotInstService.getInstancesGroupId().pipe(
-                    // get full tag and groupId from SpotInst API for APP/DB instances of specific tag
-                    concatMap(res => {
-                      for (let i = 0; i < res.response.items.length; i++) {
-                        if (res.response.items[i].name.includes(vm.id)) {
-                          this.spotInstancesPartial.push({tag: res.response.items[i].name, groupId: res.response.items[i].id});
-                        }
-                      }
-                      // get statefulId from SpotInst API for APP/DB instances of specific groupId
-                      return this.spotInstService.getInstanceInfoByGroupId(this.spotInstancesPartial[0].groupId).pipe(
-                        concatMap((res: any) => {
-                          this.spotInstancesFull.push({tag: this.spotInstancesPartial[0].tag, groupId: this.spotInstancesPartial[0].groupId, statefulId: res.response.items[0].id});
-                          return this.spotInstService.getInstanceInfoByGroupId(this.spotInstancesPartial[1].groupId).pipe(
-                            concatMap((resp: any) => {
-                              this.spotInstancesFull.push({tag: this.spotInstancesPartial[1].tag, groupId: this.spotInstancesPartial[1].groupId, statefulId: resp.response.items[0].id});
-                              return this.spotInstService.instancesGroupIdToFile(this.spotInstancesFull);
-                            }),
-                          );
-                        }),
-                      );
+    this.addVm(this.vm);
+
+    this.vm = new Vm();
+  }
+
+  addVm(vm : Vm) {
+    this.loading = true
+    this.crudService.addVm(vm).subscribe(
+      {
+        next: res => {
+          this.spotInstService.getInstancesGroupId().pipe(
+            // get full tag and groupId from SpotInst API for APP/DB instances of specific tag
+            concatMap(res => {
+              for (let i = 0; i < res.response.items.length; i++) {
+                if (res.response.items[i].name.includes(vm.id)) {
+                  this.spotInstancesPartial.push({tag: res.response.items[i].name, groupId: res.response.items[i].id});
+                }
+              }
+              // get statefulId from SpotInst API for APP/DB instances of specific groupId
+              return this.spotInstService.getInstanceInfoByGroupId(this.spotInstancesPartial[0].groupId).pipe(
+                concatMap((res: any) => {
+                  this.spotInstancesFull.push({tag: this.spotInstancesPartial[0].tag, groupId: this.spotInstancesPartial[0].groupId, statefulId: res.response.items[0].id});
+                  return this.spotInstService.getInstanceInfoByGroupId(this.spotInstancesPartial[1].groupId).pipe(
+                    concatMap((resp: any) => {
+                      this.spotInstancesFull.push({tag: this.spotInstancesPartial[1].tag, groupId: this.spotInstancesPartial[1].groupId, statefulId: resp.response.items[0].id});
+                      return this.spotInstService.instancesGroupIdToFile(this.spotInstancesFull);
                     }),
-                  ).subscribe({
-                    next: res => {
-                      console.log("Instances was written to the instances.json file")
-                      this.loading = false
-                      this.closeDialog()
-                    },
-                    error: err => console.log(err)
-                  });
-                },
-                error: err => console.log(err)
-            }
-        );
+                  );
+                }),
+              );
+            }),
+          ).subscribe({
+            next: res => {
+              console.log("Instances was written to the instances.json file")
+              this.loading = false
+              this.closeDialog()
+            },
+            error: err => console.log(err)
+          });
+        },
+        error: err => console.log(err)
+      }
+    );
 
-        this.spotInstancesPartial = [];
-        this.spotInstancesFull = [];
-      
-    }
+    this.spotInstancesPartial = [];
+    this.spotInstancesFull = [];
 
-    closeDialog() {
-        this.dialogRed.closeAll();
-    }
+  }
 
-    enableFlag() {
-        this.isEnabled = true
-    }
+  closeDialog() {
+    this.dialogRed.closeAll();
+  }
 
-    disableFlag() {
-        this.isEnabled = false
-    }
+  enableFlag() {
+    this.isEnabled = true
+  }
+
+  disableFlag() {
+    this.isEnabled = false
+  }
 }
